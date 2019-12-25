b0fa9bd7233fc337a43602d5ffa80c3027ca7b43
Coolshiv30 <51670750+Coolshiv30@users.noreply.github.com>
2019-12-25 18:37:18 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,3 +1,6 @@
+#whatsapp automation
+# Here, with the help of python code we will write text- messages to our friends or other members in whatsapp
+# Here, User has scan QR-Code of their whatsapp web  
 from selenium import webdriver
 
 driver = webdriver.Chrome('D:/chromedriver/chromedriver.exe')
@@ -10,8 +13,10 @@ input("enter anything after scanning QR code")
 
 user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 user.click()
-msg_box = driver.find_element_by_class_name("_3u328" )
+msg_box = driver.find_element_by_class_name("_3u328" ) #class_name("3u328") is different for different user who try to run this code. At first they have to open their whatsapp in any browser then they have to right click the message box and then click on inspect then again right click the message box and click on inspect then they have to write the class name from the <div tag> that is selected that is selected here. 
 for i in range(count):
     msg_box.send_keys(msg)
-    button = driver.find_element_by_class_name("_3M-N-")
+    button = driver.find_element_by_class_name("_3M-N-")#class_name("_3M-N-") is different for different user who try to run this code. At first they have to open their whatsapp in any browser then they have to right click the message box and then click on inspect then again right click on message box and click on inspect then findout the tag that starts with <button> and copy the class name of that tag here.
+
+
     button.click()
